208c2292041921022e97a3506dd4101fe210eb9a
sruthi <sruthikallingal@gmail.com>
2023-03-10 17:03:20 +0530

    about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,6 +19,18 @@ function About() {
               <img src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' ></img>
           </div>
           <div className='about'>Why Choose me ?</div>
+          <div className='desc'>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
+          </div>
+          <div className='features'>Here is Few Features:</div>
+          <div >
+            <div className='li'></div>
+            <div className='liData'>User Interface Design</div>
+          </div>
+          <div>
+            <div className='li2'></div>
+            <div className='liData2'>dhfudg fhjgfik</div>
+          </div>
         </div>
     </div>
   )
